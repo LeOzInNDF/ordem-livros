8a337a10efb35756db6f7c171dd1d87d310448f7
LeOzInNDF <leo_albano@hotmail.com>
2024-12-10 23:55:58 -0300

    feat: add series

---
+++ src/components/serie/ItemLivro.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let livro: any = null;
+</script>
+
+{#if livro}
+<a href="{livro.link}" target="_blank" class="flex flex-col rounded-md overflow-hidden bg-zingc-800">
+    <img src="{livro.imagem}" alt="Imagem do Livro" class="h-96">
+    <div class="flex-1 flex flex-col gap-2 p-5">
+        <span class="flex-1 font-bold text-center">{livro.titulo}</span>
+        <button class="botao azul"> Link do Livro </button>
+    </div>
+</a>
+{/if}
